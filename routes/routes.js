76e3b53329c89b2657639d488048d965a6c63d83
Italo Rodrigues <italo.pa61@gmail.com>
2024-03-27 16:18:44 -0300

    atividade4

--- routes/routes.js
+++ routes/routes.js
@@ -3,10 +3,12 @@ const router = express.Router() /* ROTEADOR */
 
 const lista1 = require('./lista1')
 const lista2 = require('./lista2')
+const lista3 = require('./lista3')
 const introducao = require('./introducao')
 
 router.use('/lista1', lista1)
 router.use('/lista2', lista2)
+router.use('/lista3', lista3)
 router.use('/introducao', introducao)
 
 module.exports = router
